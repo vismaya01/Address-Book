1d2d8440c31b56288419352943bc4a29ece709d2
vismaya01 <kvvismaya98@gmail.com>
2020-12-03 22:24:34 +0530

    Added: created a contact

---
+++ AddressBook.js
@@ -0,0 +1,30 @@
+var prompt = require('prompt-sync')();
+class Contact {
+
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    toString() {
+        return "First name: " + this.firstName + "\nLast name: " + this.lastName + "\nAddress: " + this.address + "\nCity: " + this.city + "\nState: " + this.state + "\nZip: " + this.zip + "\nPhone number: " + this.phoneNumber + "\nEmail: " + this.email;
+    }
+}
+
+let contact = new Contact("Vismaya","Rameshan","Kvhouse","Kannur","Kerala",12345,9775123451,"kvvismaya@gmail.com");
+console.log(contact.toString());
