8f77e8435cb3bca6b4ca9764e77c0b087863b742
vismaya01 <kvvismaya98@gmail.com>
2020-12-06 22:04:46 +0530

    Added: Search contact by city or state

--- AddressBook.js
+++ AddressBook.js
@@ -139,10 +139,21 @@ let deletContact=()=>{
         console.log("Contact is deleleted from Addressbook.")
     }
 }
+
+function searchByCityOrState(searchCityOrState, choice){
+    let contacts = new Array();
+    if(choice == 1){
+        contacts = contactArray.filter(contact => contact.city === searchCityOrState)
+    }
+    if(choice == 2){
+        contacts = contactArray.filter(contact => contact.state === searchCityOrState)
+    }
+    console.log("Contact: ",contacts);
+}
   
 let choice = 0;
 do{
-    console.log("Press: \n1) Add Contact  \n2) Edit Contact \n3) View Contact  \n4) Delete Contact \n5) Count Contacts \n0)Exit:");
+    console.log("Press: \n1) Add Contact  \n2) Edit Contact \n3) View Contact  \n4) Delete Contact \n5) Count Contacts \n6) Search City or State \n0)Exit:");
     choice = Number(prompt("Enter your choice: "));
     switch(choice){
         case 1:
@@ -165,5 +176,17 @@ do{
         case 5:
             console.log("Number of Contacts: "+contactArray.reduce(contact=>contact + 1, 0));
             break;
+        case 6:
+                console.log("1) Search By City  2) Search By State");
+                let ch = Number(prompt("Enter your choice: "));
+                switch (ch){
+                    case 1: let city = prompt("Enter the city name: ");
+                            searchByCityOrState(city, 1);
+                            break;
+                    case 2: let state = prompt("Enter the state name: ");
+                            searchByCityOrState(state, 2);
+                            break;
+                }
+                break;
     }
 }while(choice != 0);
